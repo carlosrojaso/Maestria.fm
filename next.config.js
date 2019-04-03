6f5d38daef839d6818b366c36eaf6a4310fd527a
Carlos Rojas <carlos.rojas@localhost>
2019-04-03 11:29:11 -0500

    fix(*):

--- next.config.js
+++ next.config.js
@@ -2,6 +2,14 @@ const path = require('path');
 const glob = require('glob');
 
 module.exports = {
+  exportPathMap: () => {
+    const pathMap = {}
+    glob.sync('pages/**/*.js', { ignore: 'pages/_document.js' }).forEach(s => {
+      const path = s.split(/(pages|\.)/)[2].replace(/^\/index$/, '/')
+      pathMap[path] = { page: path }
+    })
+    return pathMap
+  },
   webpack: (config, { dev }) => {
     config.module.rules.push(
       {
